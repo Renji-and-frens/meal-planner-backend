02441feebf6ff68f7f8ee33d1ffe65fb79295d1b
LongPhamHai <maxwelfrice06@gmail.com>
2024-03-18 12:37:11 +1100

    #1 initial commit

---
+++ MPWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using MPWebApi.Models;
+
+namespace MPWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.Log(LogLevel.Information, "GetEndpoint reached! Returning a response....");
+
+            //you can add logic for a service to retrieve database records, cloud storage, etc. instead of dummy data like shown
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("2")]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            _logger.Log(LogLevel.Information, "GetEndpoint2 reached! Returning a response....");
+
+            //you can add logic for a service to retrieve database records, cloud storage, etc. instead of dummy data like shown
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
