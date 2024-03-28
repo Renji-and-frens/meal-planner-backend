9c6d5c5e1360900d5d730a05cb885d1fe62209b3
LongPhamHai <62007628+yuuni-mikawane@users.noreply.github.com>
2024-03-29 01:08:20 +1100

    #11 initial commit for issue

---
+++ BasedCookingRecipeParser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasedCookingRecipeParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running based.cooking Recipe Parser...");
+
+        }
+    }
+}
