2d429bff37d4b00d96b6c79dd7b0c56a16194a9e
LongPhamHai <62007628+yuuni-mikawane@users.noreply.github.com>
2024-03-29 01:14:31 +1100

    #11 task assignment for Son

--- BasedCookingRecipeParser/Program.cs
+++ BasedCookingRecipeParser/Program.cs
@@ -7,7 +7,19 @@ namespace BasedCookingRecipeParse
         static void Main(string[] args)
         {
             Console.WriteLine("Running based.cooking Recipe Parser...");
+            Console.WriteLine("Please paste the path to the folder that has all the based.cooking recipe files to be parsed: ");
 
+            var recipeParentPath = Console.ReadLine();
+
+            Console.WriteLine($"Checking folder {recipeParentPath}...");
+            List<string> recipePaths = new List<string>();
+            //find all recipes in parent folder and append into recipePaths
+
+
+
+
+
+            //read each recipe
         }
     }
 }
